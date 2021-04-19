88775b706f65f08bf96d7af4282b09f7e1ec2476
feras80 <61097790+feras80@users.noreply.github.com>
2021-04-18 21:38:25 -0500

    Initial checkin

---
+++ Run.cmd
@@ -0,0 +1,9 @@
+cd C:\Users\cb05314\Desktop\FaceMaskDetection-master\FaceMaskDetection-master
+
+Python
+
+conda activate env_pytorch
+
+python pytorch_infer.py --img-mode 0 --video-path C:\Users\cb05314\Desktop\FaceMaskDetection-master\FaceMaskDetection-master\img\video1.mp4
+
+pause
